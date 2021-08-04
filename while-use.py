d4fc6f51a589bd5f41f77046984257792ef55fe6
nerd ops <88443860+nerd-ops@users.noreply.github.com>
2021-08-04 10:52:41 -0500

    Create while-use.py

---
+++ while-use.py
@@ -0,0 +1,33 @@
+# HI I'M NERD-OPS AND I CAN TEACH U
+# IF U HAVE A QUESTION U CAN MADE UR QUESTION IN MY TELEGRAM
+# @elx_rip
+
+# okay ,let's go
+
+# how we can make a "while" loop?
+
+# okay ,this question is too easy :3
+# first, what we can say of this loop?
+# It is infinite but we can give it an end
+#-------------------------------------------------------------
+while True: # this loop don't have a final
+  # ur code
+  pass
+while True:
+  break # this word break the "while" loop
+while True:
+  continue # It is used to return to the beginning of the loop at any time, without executing the lines below the wor
+#--------------------------------------------------------------
+# we can use conditions for "while"
+number = 90
+while number < 100: # in this case the code finish when number is greater than 100
+  number+=1
+# the "<" indicates the condition in which this loop is executed
+
+# code by : nerd-ops
+
+#    ╻ ╻   ┏━┓   ┏━┓   ┏━╸   ╻ ╻┏━┓┏━┓╺┳┓   ┏━┓╻ ╻┏━┓
+#    ┃ ┃   ┣┳┛   ┣━┫   ┣╸ ╺━╸┃╻┃┃ ┃┣┳┛ ┃┃   ┃ ┃┃╻┃┃ ┃
+#    ┗━┛   ╹┗╸   ╹ ╹   ╹     ┗┻┛┗━┛╹┗╸╺┻┛   ┗━┛┗┻┛┗━┛
+
+# BYE................
